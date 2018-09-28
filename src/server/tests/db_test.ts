e2afbdde075243a12a0d3fdbd8255718431b9672
jayphbee <jayphbee@163.com>
2018-09-28 18:04:05 +0800

    PT doesn't support console.assert

--- src/server/tests/db_test.ts
+++ src/server/tests/db_test.ts
@@ -4,11 +4,13 @@ import { Type } from "../../pi/struct/sinfo";
 export const test_db = () => {
     let memBucket = createPersistBucket("hello", Type.I32, Type.I32);
     memBucket.put(1, 2);
-    console.assert(memBucket.delete(1) === true, "delete failed");
+    // console.log(memBucket.get(1));
+    // console.assert(memBucket.delete(1) === true, "delete failed");
+
     memBucket.update(1, 11);
 
     let persistBucket = createMemoryBucket("hello", Type.I32, Type.I32);
-    persistBucket.put(1, 2);
-    console.assert(persistBucket.delete(1) === true, "delete failed");
+    persistBucket.put(11, 22);
+    // console.log(persistBucket.get(11));
+    // console.assert(persistBucket.delete(1) === true, "delete failed");
 }
-
