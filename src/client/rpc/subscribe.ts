5feda080a349d29f89f49aa8cf756bd1645b17be
gary <gary@gaia.world>
2018-10-17 18:04:08 +0800

    CHAT-4 前后端数据通讯接口

---
+++ src/client/rpc/subscribe.ts
@@ -0,0 +1,17 @@
+/**
+ * 描述了要定义的主题
+ */
+
+
+ /**
+  * 
+ims/group/msg/_hid //特定群组的聊天主题
+img/group/anounnce/_aid //特定群组的公告
+ims/user/msg/_hid //和特定用户的聊天主题
+ims/user/inbox/applyFriend  //别人申请添加当前用户为好友
+ims/user/inbox/acceptFriend //别人同意添加当前用户为好友
+ims/user/inbox/inviteJoin //别人邀请当前用户加入群
+ims/user/inbox/acceptJoin //群接受了当前用户的加入申请
+
+news/暂无
+  */
