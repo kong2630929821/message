6197b879f0a1ee93f9cd2d51dd9b55e65a6c7331
gary <gary@gaia.world>
2018-10-18 11:32:19 +0800

    CHAT-4 前后端数据通讯接口

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,9 @@
-set NODE_PATH="..\..\pi_build\"
+cd ..
+if exist .git\hooks\commit-msg goto :compile
+copy /Y jira_script\jira_git\rely\commit-msg .git\hooks
+goto :compile
+
+:compile
+cd .\scripts
 ..\..\pi_build\bin\node\node ..\..\pi_build\dst\init.js
 pause;
