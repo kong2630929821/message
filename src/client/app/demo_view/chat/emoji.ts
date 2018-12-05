5faa4d7e07b4b2293530ab14aa76dcbe176f173a
tangm <gary@gaia.world>
2018-12-05 18:00:14 +0800

    CHAT-72	支持表情

---
+++ src/client/app/demo_view/chat/emoji.ts
@@ -0,0 +1,47 @@
+/**
+ * 单人聊天
+ */
+
+// ================================================ 导入
+import { notify } from '../../../../pi/widget/event';
+import { Widget } from '../../../../pi/widget/widget';
+import { Logger } from '../../../../utils/logger';
+
+// tslint:disable-next-line:no-reserved-keywords
+declare var module;
+const WIDGET_NAME = module.id.replace(/\//g, '-');
+const logger = new Logger(WIDGET_NAME);
+
+// ================================================ 导出
+export class Emoji extends Widget {
+    public props:Props;
+    public ok:() => void;
+    public process:(emoji:string) => void;
+    constructor() {
+        super();
+        this.props = {
+            emojis:EMOJIS
+        };
+    }
+    public back() {
+        this.ok();
+    }
+    public click(index:number) {
+        this.process(EMOJIS[index][0]);
+    }    
+}
+export let EMOJIS_MAP = new Map();
+
+// ================================================ 本地
+const EMOJIS = [
+    ['smile','../../res/emoji/smile.gif'],
+    ['angry','../../res/emoji/angry.gif']
+];
+
+EMOJIS.forEach((ele) => {
+    EMOJIS_MAP.set(ele[0],ele[1]);
+});
+
+interface Props {
+    emojis:string[][];
+}
